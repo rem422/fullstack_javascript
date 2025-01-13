e3c81e36b5eed5c1ee1c5d6c90516aacdefe5a0c
Rem Simiyu <remsimiyu@gmail.com>
2025-01-14 00:48:43 +0300

    FullStack JavaScriptnode

---
+++ Basics/premitive_data_types_challenge.js
@@ -0,0 +1,19 @@
+//1. Create Variable name (firstFavNum) & store your favourite number.
+//2. Create Variable name (secondFavNum) & store your second favourite.
+//3. Add (firstFavNum & secondFavNum).
+//4. Subtract (firstFavNum & secondFavNum).
+//5. Multiply (firstFavNum & secondFavNum).
+//6. Divide (firstFavNum & secondFavNum).
+//7. check (firstFavNum Mod secondFavNum).
+//8. check the power of  (firstFavNum).
+
+let firstFavNum = 4;
+let secondFavNum = 2;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** firstFavNum);
+
